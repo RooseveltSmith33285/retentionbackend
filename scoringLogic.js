3d41d104ff161847f621f5823c12aabe2077ebc8
DawrAli33285 <lemightyeagle@gmail.com>
2025-05-08 16:28:21 +0500

    new

--- scoringLogic.js
+++ scoringLogic.js
@@ -4,7 +4,9 @@ const keywordData = require('./risk_keywords.json');
 const API_KEY = '49174427b558d2af53e538f950d775f5';
 const BASE_URL = 'https://api.social-searcher.com/v2/search';
 
+const peopledatalabs =require('@api/peopledatalabs');
 
+peopledatalabs.auth('30d80327aac2828dd4df86eaf9ec379dd5bae8d495490b2c41f4f313ca34adea');
 function decayWeight(n) {
   const base = [1.0, 0.5, 0.25];
   const additional = Math.max(n - 3, 0);
@@ -14,71 +16,113 @@ function decayWeight(n) {
     .reduce((a, b) => a + b, 0);
 }
 
-async function fetchMentions(keyword, email, company) {
-
-  // const requests = [
-  //   { network: 'linkedin' },
-  //   { network: 'facebook' },
-  //   { network: 'twitter' },
-  //   {network:'web'}
-  // ].map(config => {
-  //   const params = {
-  //     q: `${email} OR ${company}`, 
-  //     key: API_KEY,
-  //     network: config.network,
-  //     lang: 'en'
-  //   };
-  //   return axios.get(BASE_URL, { params });
-  // });
-
-  try {
-   
-    // const responses = await Promise.all(requests);
-    
-    // const allPosts = responses.flatMap(response => 
-    //   response.data.posts || []
-    // );
-
-   
-    // const keywordLower = keyword.toLowerCase();
-    // console.log("all posts")
-    // console.log(allPosts)
-    // return allPosts.filter(post => 
-    //   post.text?.toLowerCase().includes(keywordLower)
-    // );
-
-    // const options = {
-    //   method: 'GET',
-    //   url: 'https://linkedin-data-api.p.rapidapi.com/get-profile-posts',
-    //   params: {
-    //     username: 'syed-dawar-ali-bukhari-81668822a'
-    //   },
-    //   headers: {
-    //     'x-rapidapi-key': 'aeb3201686msh526f7a596c6ec1bp14536cjsn183119f6a825',
-    //     'x-rapidapi-host': 'linkedin-data-api.p.rapidapi.com'
-    //   }
-    // };
-
-    // const response = await axios.request(options);
-    
-  // const params = {
-  //   q: `${email} OR ${company}`,
-  //   key: API_KEY,
-  //   network: 'linkedin',
-  // };
-
+async function fetchMentions(keyword, socialMedia) {
+    try {
+      const allPosts = [];
+      
+      
+      if (socialMedia.linkedin_username) {
+        const linkedinOptions = {
+          method: 'GET',
+          url: 'https://linkedin-data-api.p.rapidapi.com/get-profile-posts',
+          params: { username: socialMedia.linkedin_username },
+          headers: {
+            'x-rapidapi-key': '39344189b5msh21c51f47d338fabp10dcfejsn510da5694dd8',
+            'x-rapidapi-host': 'linkedin-data-api.p.rapidapi.com'
+          }
+        };
   
-  // const res = await axios.get(BASE_URL, { params });
-  // console.log(res.data)
+        const linkedinResponse = await axios.request(linkedinOptions);
+        const linkedinPosts = linkedinResponse.data.data
+          .filter(post => post?.text || post?.resharedPost?.text)
+          .map(post => ({
+            text: post.text || post.resharedPost.text,
+            network: 'linkedin'
+          }));
+        allPosts.push(...linkedinPosts);
+      }
   
-  // return res.data.posts.filter(post =>
-  //   post.text.toLowerCase().includes(keyword.toLowerCase())
-  // );
-  } catch (error) {
-    console.error('API Error:', error.message);
-    return [];
+     
+      if (socialMedia.twitter_username) {
+        const twitterOptions = {
+          method: 'GET',
+          url: 'https://twitter241.p.rapidapi.com/user',
+          params: { username: socialMedia.twitter_username },
+          headers: {
+            'x-rapidapi-key': '39344189b5msh21c51f47d338fabp10dcfejsn510da5694dd8',
+            'x-rapidapi-host': 'twitter241.p.rapidapi.com'
+          }
+        };
+  
+        const twitterResponse = await axios.request(twitterOptions);
+        const userId = twitterResponse.data.result.data.user.result.rest_id;
+  
+        const twitterPostOptions = {
+          method: 'GET',
+          url: 'https://twitter241.p.rapidapi.com/user-tweets',
+          params: { user: userId, count: '20' },
+          headers: {
+            'x-rapidapi-key': '39344189b5msh21c51f47d338fabp10dcfejsn510da5694dd8',
+            'x-rapidapi-host': 'twitter241.p.rapidapi.com'
+          }
+        };
+  
+        const twitterPostResponse = await axios.request(twitterPostOptions);
+        const twitterPosts = twitterPostResponse.data.result.timeline.instructions
+          .find(i => i.type === "TimelineAddEntries")?.entries
+          ?.filter(entry => entry.entryId?.startsWith("tweet-"))
+          .map(entry => ({
+            text: entry.content.itemContent.tweet_results.result.legacy.full_text,
+            network: 'twitter'
+          })) || [];
+        allPosts.push(...twitterPosts);
+      }
+  
+  
+      if (socialMedia.facebook_username) {
+        const facebookOptions = {
+          method: 'GET',
+          url: 'https://facebook-scraper3.p.rapidapi.com/profile/details_url',
+          params: { url: socialMedia.facebook_url || `https://facebook.com/${socialMedia.facebook_username}` },
+          headers: {
+            'x-rapidapi-key': '39344189b5msh21c51f47d338fabp10dcfejsn510da5694dd8',
+            'x-rapidapi-host': 'facebook-scraper3.p.rapidapi.com'
+          }
+        };
+  
+        const facebookResponse = await axios.request(facebookOptions);
+        const profileId = facebookResponse.data.profile.profile_id;
+  
+        const facebookPostOptions = {
+          method: 'GET',
+          url: 'https://facebook-scraper3.p.rapidapi.com/profile/posts',
+          params: { profile_id: profileId },
+          headers: {
+            'x-rapidapi-key': '39344189b5msh21c51f47d338fabp10dcfejsn510da5694dd8',
+            'x-rapidapi-host': 'facebook-scraper3.p.rapidapi.com'
+          }
+        };
+  
+        const facebookPostResponse = await axios.request(facebookPostOptions);
+        const facebookPosts = facebookPostResponse.data.results
+          .filter(post => post.message)
+          .map(post => ({
+            text: post.message,
+            network: 'facebook'
+          }));
+        allPosts.push(...facebookPosts);
+      }
+
+    
+      return allPosts.filter(post =>
+        post.text.toLowerCase().includes(keyword.toLowerCase())
+      );
+    } catch (error) {
+      console.error('API Error:', error.message);
+      return [];
+    }
   }
-}
+  
 
 async function processEmployees(employees) {
   const results = [];
@@ -91,160 +135,56 @@ async function processEmployees(employees) {
         'job satisfaction': 0,
         'family & work-life balance': 0,
         'communication & leadership': 0,
-        'Lack of rest':0
+        'Lack of rest':0,       
       };
   let employeeName=emp['Employee Name (Last Suffix, First MI)']
 
   let splitName=employeeName.split(',')
   let firstName=splitName[0].trim()
   let lastName=splitName[1].trim()
+  let email=emp['E-mail Address']?emp['E-mail Address']:emp['Alternate Email']
   
   let companyName=emp['Company Name']?emp['Company Name']:emp['Company ']
   
   
       const phraseMap = {};
-      const optionsone = {
-        method: 'GET',
-        url: 'https://linkedin-data-api.p.rapidapi.com/search-people',
-        params: {
-          firstName: firstName,
-          lastName:lastName,
-          company:companyName,
-          postedAt:getLast90Days()
-           
-  
-        },
-        headers: {
-          'x-rapidapi-key': '948db025d8msh9ec8678e6cae89fp1e7a80jsn077682701341',
-          'x-rapidapi-host': 'linkedin-data-api.p.rapidapi.com'
-        }
-      };
-      
+   let data=await peopledatalabs.getV5PersonEnrich({
+        email,
+        name:employeeName,
+        company:companyName,
      
-      const responseone = await axios.request(optionsone);
-      console.log('username')
-      console.log(responseone.data)
-      console.log(responseone.data?.data?.items)
-      
-    //   let contains = responseone?.data?.data?.items?.filter(u => 
-    //     u?.fullName?.toLowerCase().includes("riaz")
-    //   );
-  
-    if(responseone.data?.data?.items==null){
-    continue
-    }
-    let username=responseone.data?.data?.items[0]?.username;
-     const optionstwo = {
-      method: 'GET',
-      url: 'https://linkedin-data-api.p.rapidapi.com/get-profile-posts',
-      params: {
-        username: username,
-      },
-      headers: {
-        'x-rapidapi-key': '948db025d8msh9ec8678e6cae89fp1e7a80jsn077682701341',
-        'x-rapidapi-host': 'linkedin-data-api.p.rapidapi.com'
+      })
+
+
+      if(!data?.data?.data){
+continue;
       }
-    };
+     
+      const socialMedia = {
+        linkedin_url: data?.data?.data?.linkedin_url || null,
+        linkedin_username: data?.data?.data?.linkedin_username || null,
+        twitter_url: data?.data?.data?.twitter_url || null,
+        twitter_username: data?.data?.data?.twitter_username || null,
+        facebook_url: data?.data?.data?.facebook_url || null,
+        facebook_username: data?.data?.data?.facebook_username || null
+      };
     
-      const responsetwo = await axios.request(optionstwo);
-    console.log("linkedin posts")
-      console.log(responsetwo.data.data)
-      if(!responsetwo?.data?.data){
-continue
-      }
-  //   const twitteroptionsone =  {
-  //     method: 'GET',
-  //     url: 'https://twitter135.p.rapidapi.com/Search/',
-  //     params: {
-  //       q: 'Tina Abrams',
-  //       count: '10',
-  //       type: 'People',
-  //       safe_search: 'false'
-  //     },
-  //     headers: {
-  //       'x-rapidapi-key': '06f131c757mshe6d549d8f59da3ep198b32jsn22106f38098f',
-  //       'x-rapidapi-host': 'twitter135.p.rapidapi.com'
-  //     }
-  //   };
-  //   const responseone = await axios.request(twitteroptionsone);
-  //   const firstMemberLegacy = responseone.data;
-  //   let allUsers=firstMemberLegacy.data.search_by_raw_query.search_timeline.timeline.instructions[1].entries;
-  
-  //   allUsers?.map((val,i)=>{
-  //     console.log(val?.content?.itemContent?.user_results?.result?.legacy)
-  //   })
-  //   // return;
-    
-  //   let matchedUser=allUsers.filter(val=>val?.content?.itemContent?.user_results?.result?.legacy?.name?.includes('tina'))
-  // let username=matchedUser[1]?.content?.itemContent?.user_results?.result?.legacy?.screen_name;
-  
-  
-  // const twitteroptionsthree = {
-  //   method: 'GET',
-  //   url: 'https://twitter135.p.rapidapi.com/v2/UserByScreenName/',
-  //   params: {
-  //     username:'correa_rebeca'
-  //   },
-  //   headers: {
-  //     'x-rapidapi-key': '06f131c757mshe6d549d8f59da3ep198b32jsn22106f38098f',
-  //     'x-rapidapi-host': 'twitter135.p.rapidapi.com'
-  //   }
-  // };
-  
-  // const twitterResponseThree = await axios.request(twitteroptionsthree);
-  
-  // console.log(twitterResponseThree.data.data.user.result.rest_id)
-  // return;
-  // let userId=twitterResponseThree.data.data.user.result.rest_id
-  // const options = {
-  //   method: 'POST',
-  //   url: 'https://social-media-email-scraper-2025.p.rapidapi.com/v2/get_email',
-  //   headers: {
-  //     'x-rapidapi-key': '06f131c757mshe6d549d8f59da3ep198b32jsn22106f38098f',
-  //     'x-rapidapi-host': 'social-media-email-scraper-2025.p.rapidapi.com',
-  //     'Content-Type': 'application/json'
-  //   },
-  //   data: {
-  //     platform: 'twitter',
-  //     username: username,
-  //   }
-  // };
-  
-  
-  // const response = await axios.request(options);
-  // console.log(response.data)
-  
-  
-  
-  
-  // const twitteroptionstwo={
-  //   method: 'GET',
-  //   url: 'https://twitter241.p.rapidapi.com/user-tweets',
-  //   params: {
-  //     user: userId,
-  //     count: '20'
-  //   },
-  //   headers: {
-  //    'X-Rapidapi-Key' :'06f131c757mshe6d549d8f59da3ep198b32jsn22106f38098f',
-  //     'x-rapidapi-host': 'twitter241.p.rapidapi.com'
-  //   }
-  // };
-  // const twitterResponse = await axios.request(twitteroptionstwo);
-  
-  // console.log(twitterResponse.data.result?.timeline?.instructions[1]?.entries[0])
-   
+     
   
       for (const keyword of keywordData) {
-        const matches = responsetwo.data.data.filter(u => 
-          u.text?.toLowerCase().includes(keyword.Phrase?.toLowerCase()) 
-        );
-        console.log(matches)
+        // const matches = responsetwo.data.data.filter(u => 
+        //   u.text?.toLowerCase().includes(keyword.Phrase?.toLowerCase()) 
+        // );
+       
+        const matches = await fetchMentions(keyword.Phrase, socialMedia);
+        
+        
         if (matches?.length > 0) {
-          const count = matches?.length;
-          const weighted = keyword.RiskScore * decayWeight(count);
-          phraseMap[keyword.Category] = (phraseMap[keyword.Category] || 0) + weighted;
+            const count = matches.length;
+            const weighted = keyword.RiskScore * decayWeight(count);
+            phraseMap[keyword.Category] = (phraseMap[keyword.Category] || 0) + weighted;
+          }
         }
-      }
   
       let totalScore = 0;
       for (const [cat, rawScore] of Object.entries(phraseMap)) {
@@ -266,8 +206,6 @@ continue
         riskLevel,
       });
     }catch(e){
-      console.error(`Error processing ${emp['Employee Name']}:`, error);
-      // Push empty result to maintain array length
       results.push(createDefaultResult(emp));
     }
   }
